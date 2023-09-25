4e95c69c953ce076a6842eb2859e1117a392829c
FT9R <ft9r@mail.ru>
2023-09-25 11:27:00 +0300

    Some SPI error handling

--- AT45/AT45_Interface.c
+++ AT45/AT45_Interface.c
@@ -3,7 +3,8 @@
 void AT45_SPI_Transmit(SPI_HandleTypeDef *hspix, uint8_t *pData, uint16_t size, uint32_t timeout)
 {
 #ifdef USE_HAL_DRIVER
-    HAL_SPI_Transmit(hspix, pData, size, timeout);
+    if (HAL_SPI_Transmit(hspix, pData, size, timeout) != HAL_OK)
+        Error_Handler();
 #else
     if (SPI_Transmit(hspix, pData, size, timeout) != SPI_STATE_READY)
         Error_Handler();
@@ -13,7 +14,8 @@ void AT45_SPI_Transmit(SPI_HandleTypeDef *hspix, uint8_t *pData, uint16_t size, 
 void AT45_SPI_Receive(SPI_HandleTypeDef *hspix, uint8_t *pData, uint16_t size, uint32_t timeout)
 {
 #ifdef USE_HAL_DRIVER
-    HAL_SPI_Receive(hspix, pData, size, timeout);
+    if (HAL_SPI_Receive(hspix, pData, size, timeout) != HAL_OK)
+        Error_Handler();
 #else
     if (SPI_Receive(hspix, pData, size, timeout) != SPI_STATE_READY)
         Error_Handler();
